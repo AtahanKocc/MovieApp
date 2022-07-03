03c5d714273d5cf49954319657b4b3f4f1a5f054
Atahan Koc <koccata@gmail.com>
2022-07-03 17:54:15 +0300

    Lesson1: Generating Component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'movies',
+    templateUrl: './movies.component.html'
+})
+export class MoviesComponent{
+
+}
+
+
+
+// selector:'.movies'  // <div class="movies"> 
+// selector: 'movies' //  <movies></movies>
+// selector: '#movies' // <div id="movies">
+// bu olusturdugumuz componentten appmodule un haberi olması lazım, app module git.
